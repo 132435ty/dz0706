d272f0215c5ab3cb6064f7c7f66ae273416fba6e
132435ty <Kz89267908477@gmail.com>
2024-06-10 09:43:04 +0300

    10 06 24

--- app/src/main/java/com/grobik/myapplication/MainActivity.kt
+++ app/src/main/java/com/grobik/myapplication/MainActivity.kt
@@ -21,5 +21,19 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity3::class.java)
             startActivity(intent)
         }
+
+        var i = 0
+        binding.button3.setOnClickListener {
+            if (i == 0) {
+                binding.textView3.text = "Вжух"
+                i = 1
+            } else if (i == 1) {
+                binding.textView3.text = "Привет"
+                i = 2
+            } else {
+                binding.textView3.text = "нажми↓↓↓↓"
+                i = 0
+            }
+        }
     }
 }
